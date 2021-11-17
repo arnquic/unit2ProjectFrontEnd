106a3ea47cfd01da435269fbb3b23948f8f46e84
Cullen REDDY <cull.redd@gmail.com>
2021-11-17 15:27:04 -0800

    fixed signup and login for main.js

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const app = express();
-const routesReport = require('rowdy-logger').being(app);
-const path = require('path');
+const express = require('express')
+const app = express()
+
+const routesReport = require('rowdy-logger').begin(app)
+
+const path = require('path')
 
 app.get('/', (req, res) => {
-    const filepath = path.join(__dirname, 'index.html');
-    res.sendFile(filepath);
-});
+  const filepath = path.join(__dirname, 'index.html')
+  res.sendFile(filepath)
+})
 
 app.get('/main.js', (req, res) => {
-    const filepath = path.join(__dirname, 'main.js');
-    res.sendFile(filepath);
-});
+  const filepath = path.join(__dirname, 'main.js')
+  res.sendFile(filepath)
+})
 
 app.get('/style.css', (req, res) => {
-    const filepath = path.join(__dirnam, 'style.css');
-    res.type('css').sendFile(filepath);
-});
+  const filepath = path.join(__dirname, 'style.css')
+  res.type('css').sendFile(filepath)
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    reoutesReport.print();
+  routesReport.print()
 })
